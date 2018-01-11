e7d26b19c7f09d3081b863f790571954b4615cde
Rodrigo Llanos <rodrigo.llanosordonez@altran.com>
2018-01-11 17:42:53 +0100

    [REF] Cambiados los estilos y escritos los stateless components en anotación funcion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -45,44 +45,43 @@ class App extends Component {
   render() {
     const { list, query } = this.state;
     return (
-      <section className="app">
-        <Search value={query} onChange={this.onSearchChange}>
-          Search
-        </Search>
-        <Table list={list} pattern={query} />
+      <section className="page">
+        <section className="interactions">
+          <Search value={query} onChange={this.onSearchChange}>
+            Search
+          </Search>
+          <Table list={list} pattern={query} />
+        </section>
       </section>
     );
   }
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children} <input type="text" value={value} onChange={onChange} />
-      </form>
-    );
-  }
-}
+const Search = ({ value, onChange, children }) => (
+  <form>
+    {children} <input type="text" value={value} onChange={onChange} />
+  </form>
+);
 
-class Table extends Component {
-
-  render() {
-    const { list, pattern } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item =>
-          <div key={item.objectID}>
-            <span><a href={item.url}>{item.title}</a></span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        )}
+const Table = ({ list, pattern }) => (
+  <div className="table">
+    {list.filter(isSearched(pattern)).map(item =>
+      <div key={item.objectID} className="table-row">
+        <span style={{ width: '40%' }}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={{ width: '30%' }}>
+          {item.author}
+        </span>
+        <span style={{ width: '15%' }}>
+          {item.num_comments}
+        </span>
+        <span style={{ width: '15%' }}>
+          {item.points}
+        </span>
       </div>
-    )
-  }
-}
+    )}
+  </div>
+);
 
 export default App;
